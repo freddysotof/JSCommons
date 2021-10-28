74e364b0b389d6585072eaf93da3344208ff2343
Freddy Soto <45477077+freddysotof@users.noreply.github.com>
2021-10-28 11:00:25 -0400

    Update autoclicker.js

--- autoclicker.js
+++ autoclicker.js
@@ -1,7 +1,7 @@
 const autoClick = (classId,position)=>{
   let tbody = $(classId).siblings('tbody')[0]
   let row = $('tr',tbody)[position];
-	let input = $('input',temp1);
+	let input = $('input',row);
 let botonSubmit = document.getElementById('btnGuardar')
 setInterval(()=>{
 window.alert = function() {};
