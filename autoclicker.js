5a095a0a4da060adba221ec78a74cb9cbda8a97b
Freddy Soto <45477077+freddysotof@users.noreply.github.com>
2022-01-27 10:20:53 -0400

    Cambio de clase

--- autoclicker.js
+++ autoclicker.js
@@ -1,7 +1,5 @@
 const autoClick = (classId,position)=>{
-  let tbody = $(classId).siblings('tbody')[0]
-  let row = $('tr',tbody)[position];
-	let input = $('input',row);
+	let input = $(`[name='${classId}']`)[position]
 let botonSubmit = document.getElementById('btnGuardar')
 setInterval(()=>{
 window.alert = function() {};
